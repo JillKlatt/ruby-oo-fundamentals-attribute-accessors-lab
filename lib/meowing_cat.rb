c5b5b7e30c9dd9d823e15fd83f05360aab76aa93
JillKlatt <klatt.jill@gmail.com>
2021-02-09 15:26:55 -0600

    commit tree 08d110f0475d5fc9165c6c2ae6318a1629242f8d

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
 ## code your solution here. 
+require "pry"
+
+class Cat 
+    attr_accessor :name
+    # def initialize(name)
+    # end
+
+        def meow
+            puts "meow!"
+        end
+end
+
+# binding.pry
+
+# cat = Cat.new("Maru")
+# cat.name
